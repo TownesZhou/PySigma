ef2e02b356f9f2f1b5ba4e5fb9bef670a67b1e03
Jincheng Zhou <jinchenz@usc.edu>
2020-02-04 13:16:56 -0800

    File description

--- src/Sigmaboard.py
+++ src/Sigmaboard.py
@@ -1,6 +1,6 @@
 """
     Sigmaboard, resembling the concept of Tensorboard from Tensorflow, a web-based interactive visualization tool for
-        Sigma graphical architecture.
+        Sigma graphical backend.
 """
 
 
