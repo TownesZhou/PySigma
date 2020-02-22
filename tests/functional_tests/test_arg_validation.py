b29602c730e9ecd0384e94b282c00cb6fa567869
Yunzhe Wang <yunzhewa@usc.edu>
2020-02-21 18:37:21 -0800

    add arg validation test cases

--- tests/functional_tests/test_arg_validation.py
+++ tests/functional_tests/test_arg_validation.py
@@ -51,6 +51,12 @@ class TestStructureArgValidation:
             p = PredicatePattern('test', [0, 1, 2])
 
         with pytest.raises(ValueError):
+            p = PredicatePattern('test', list(), "sigmoid")
+
+        with pytest.raises(ValueError):
+            p = PredicatePattern('test', "string", "sigmoid")
+
+        with pytest.raises(ValueError):
             test_pattern_element_1 = PatternElement("arg_1")
             test_pattern_element_2 = PatternElement("arg_2")
             p = PredicatePattern('test', [test_pattern_element_1, test_pattern_element_2, "not a pattern element"])
@@ -84,6 +90,9 @@ class TestStructureArgValidation:
 
         with pytest.raises(ValueError):
             p = PatternElement("arg_test", 2.333)
+
+        with pytest.raises(ValueError):
+            p = PatternElement("arg_test", list())
 
         with pytest.raises(ValueError):
             p = PatternElement("arg_test", [True, True, False])
@@ -193,25 +202,39 @@ class TestStructureArgValidation:
         """
             Test Type with problematic arguments
         """
-        t = Type(1, "discrete", 1, 3)
+        with pytest.raises(ValueError):
+            t = Type(1, "discrete", 1, 3)
 
-        t = Type("test", "not symbolic or discrete")
+        with pytest.raises(ValueError):
+            t = Type("test", "not symbolic or discrete")
 
-        t = Type("test", "discrete")
+        with pytest.raises(ValueError):
+            t = Type("test", "discrete")
 
-        t = Type("test", "symbolic")
+        with pytest.raises(ValueError):
+            t = Type("test", "symbolic")
 
-        t = Type("test", "discrete", min='a', max='c')
+        with pytest.raises(ValueError):
+            t = Type("test", "discrete", min='a', max='c')
 
-        t = Type("test", "discrete", min=2, max=2)
+        with pytest.raises(ValueError):
+            t = Type("test", "discrete", min=2, max=2)
 
-        t = Type("test", "discrete", min=4, max=2)
+        with pytest.raises(ValueError):
+            t = Type("test", "discrete", min=4, max=2)
 
-        t = Type("test", "discrete", min=1.0, max=3.0)
+        with pytest.raises(ValueError):
+            t = Type("test", "discrete", min=1.0, max=3.0)
 
-        t = Type("test", "symbolic", symbol_list="not a list")
+        with pytest.raises(ValueError):
+            t = Type("test", "symbolic", symbol_list="not a list")
 
-        t = Type("test", "symbolic", symbol_list=["check", "symbol", "type"])
+        with pytest.raises(ValueError):
+            t = Type("test", "symbolic", symbol_list=[])
+
+        with pytest.raises(ValueError):
+            t = Type("test", "symbolic", symbol_list=["check", "symbol", "type"])
+
 
 
     def test_type_2(self):
@@ -223,6 +246,188 @@ class TestStructureArgValidation:
         t = Type("test", "discrete", 1, 2)
 
         # t = Type("test", "symbolic" [list of symbolic type])
+
+
+    def test_predicate_1(self):
+        """
+            Test Type with problematic arguments
+        """
+        test_predicate_arg_1 = PredicateArgument('test', 'test_type', '!')
+        test_predicate_arg_2 = PredicateArgument('test', 'test_type', '=')
+        predicate_list = [test_predicate_arg_1, test_predicate_arg_2]
+
+        with pytest.raises(ValueError):
+            p = Predicate(1, [1, 2, 3])
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", list())
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", [1, 2, 3, 4])
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", predicate_list, world="semi-open")
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", predicate_list, world="semi-open")
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", predicate_list, exponential="string")
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", predicate_list, normalize="string")
+
+        with pytest.raises(ValueError):
+            p = Predicate("test", predicate_list, function=False)
+
+
+    def test_predicate_2(self):
+        """
+            Test Type with correct arguments
+        """
+        test_predicate_arg_1 = PredicateArgument('test', 'test_type', '!')
+        test_predicate_arg_2 = PredicateArgument('test', 'test_type', '=')
+        predicate_list = [test_predicate_arg_1, test_predicate_arg_2]
+
+        p = Predicate("test", predicate_list)
+
+        p = Predicate("test", predicate_list, world="closed")
+
+        p = Predicate("test", predicate_list, exponential=True)
+
+        p = Predicate("test", predicate_list, exponential=["str_1", "str_2"])
+
+        p = Predicate("test", predicate_list, normalize=True)
+
+        p = Predicate("test", predicate_list, normalize=["str_1", "str_2"])
+
+        p = Predicate("test", predicate_list, function=1)
+
+        p = Predicate("test", predicate_list, function=2.333)
+
+        p = Predicate("test", predicate_list, function="test_string")
+
+        p = Predicate("test", predicate_list, function=torch.tensor([[1., -1.], [1., -1.]]))
+
+        p = Predicate("test", predicate_list,
+                      exponential=["str_1", "str_2"],
+                      normalize=["str_1", "str_2"],
+                      perception=True,
+                      function=torch.tensor([[1., -1.], [1., -1.]]))
+
+
+    def test_conditional_1(self):
+        """
+            Test Type with problematic arguments
+        """
+        test_pattern_element_1 = PatternElement("arg_1")
+        test_pattern_element_2 = PatternElement("arg_2")
+        pattern_element_list = [test_pattern_element_1, test_pattern_element_2]
+        test_predicate_pattern_1 = PredicatePattern('test_1', pattern_element_list)
+        test_predicate_pattern_2 = PredicatePattern('test_2', pattern_element_list)
+        predicate_pattern_list = [test_predicate_pattern_1, test_predicate_pattern_2]
+
+        with pytest.raises(ValueError):
+            c = Conditional(1)
+
+        with pytest.raises(ValueError):
+            c = Conditional("test")
+
+        with pytest.raises(ValueError):
+            c = Conditional("test", conditions=predicate_pattern_list)
+
+        with pytest.raises(ValueError):
+            c = Conditional("test",
+                            conditions=predicate_pattern_list,
+                            actions=predicate_pattern_list,
+                            function_var_names=list())
+
+        with pytest.raises(ValueError):
+            c = Conditional("test",
+                            conditions=predicate_pattern_list,
+                            actions=predicate_pattern_list,
+                            function_var_names="test_string")
+
+        with pytest.raises(ValueError):
+            c = Conditional("test",
+                            conditions=predicate_pattern_list,
+                            actions=predicate_pattern_list,
+                            normal="test_string")
+
+        with pytest.raises(ValueError):
+            c = Conditional("test",
+                            conditions=predicate_pattern_list,
+                            actions=predicate_pattern_list,
+                            function=list())
+
+
+    def test_conditional_2(self):
+        """
+            Test Type with correct arguments
+        """
+        test_pattern_element_1 = PatternElement("arg_1")
+        test_pattern_element_2 = PatternElement("arg_2")
+        pattern_element_list = [test_pattern_element_1, test_pattern_element_2]
+        test_predicate_pattern_1 = PredicatePattern('test_1', pattern_element_list)
+        test_predicate_pattern_2 = PredicatePattern('test_2', pattern_element_list)
+        predicate_pattern_list = [test_predicate_pattern_1, test_predicate_pattern_2]
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list)
+
+        c = Conditional("test", actions=predicate_pattern_list)
+
+        c = Conditional("test", conducts=predicate_pattern_list)
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        conducts=predicate_pattern_list)
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        function_var_names=["str_1", "str_2"])
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        normal=["str_1", "str_2"])
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        function=1)
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        function=2.333)
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        function="test_string")
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        function=torch.tensor([[1., -1.], [1., -1.]]))
+
+        c = Conditional("test",
+                        conditions=predicate_pattern_list,
+                        conducts=predicate_pattern_list,
+                        actions=predicate_pattern_list,
+                        function_var_names=["str_1", "str_2"],
+                        function=torch.tensor([[1., -1.], [1., -1.]]),
+                        normal=["str_1", "str_2"])
+
+
+
+
+
+
 
 
 
