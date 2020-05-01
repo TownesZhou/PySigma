32b6481f15fa2262c779b7db68fbeabb5f0a6430
Jincheng Zhou <jinchenz@usc.edu>
2020-05-01 16:39:13 -0700

    Complete revision of Variable, Message, and LinkData class. Add util class for conversion between distribution and parameters. Fix mistakes of static method and variables

--- pysigma/graphical/_structures.py
+++ pysigma/graphical/_structures.py
@@ -2,10 +2,15 @@
     Basic structures in the graphical architecture
 """
 import torch
-from torch.distributions.distribution import Distribution
+from torch.distributions import Distribution
 from torch.distributions.categorical import Categorical
 from torch.distributions.constraints import Constraint
 from enum import Enum
+from collections.abc import Iterable
+from torch.distributions.kl import kl_divergence as kl
+
+from ._nodes import VariableNode, FactorNode
+from ..utils import Dist2Params, Params2Dist
 
 
 # Variable Metatypes and Variable for general inference
@@ -25,15 +30,6 @@ class Variable:
         The "type" of a variable, as defined in a predicate, is not considered here. This is to support matching across
             different types in a principle way.
     """
-    # Variable name, its identity. Used for variable matching
-    name = None
-    # Variable meta-type, of type VariableMetatype
-    metatype = None
-    # Variable size. Size of the dimension that the variable corresponds to
-    size = None
-    # Constraints of the variable's value range. Of type Constraint from PyTorch. Only set when variable type is Random.
-    constraint = None
-
     def __init__(self, name: str, metatype: VariableMetatype, size: int, constraint: Constraint):
         """
             Instantiate a variable
@@ -43,9 +39,14 @@ class Variable:
         assert isinstance(size, int)
         assert isinstance(constraint, Constraint)
 
+        # Variable name, its identity. Used for variable matching. Of type str
         self.name = name
+        # Variable meta-type, of type VariableMetatype
         self.metatype = metatype
+        # Variable size. Size of the dimension that the variable corresponds to. Of type int
         self.size = size
+        # Constraints of the variable's value range. Of type Constraint from PyTorch. Only set when variable type is
+        #   Random.
         self.constraint = constraint
 
     def __eq__(self, other):
@@ -101,20 +102,7 @@ class Message:
             torch.distribution may coexists with a particle list, whereas in a Particles message only particle list
             exists.
     """
-    # Message type, of type MessageType
-    type = None
-    # Distribution
-    dist = None
-    # Particle list
-    particles = None
-    weights = None
-    # Shapes
-    s_shape = None
-    b_shape = None
-    e_shape = None
-
-    def __init__(self, msg_type: MessageType, sample_shape: torch.Size, batch_shape: torch.Size,
-                 event_shape: torch.Size,
+    def __init__(self, msg_type: MessageType, sample_shape: torch.Size, batch_shape: torch.Size, event_shape: torch.Size,
                  dist: Distribution = None, particles: torch.Tensor = None, weights: torch.Tensor = None):
         """
             Instantiate a message
@@ -127,10 +115,14 @@ class Message:
         assert particles is None or isinstance(particles, torch.Tensor)
         assert weights is None or isinstance(weights, torch.Tensor)
 
+        # Message type, of type MessageType
         self.type = msg_type
+        # Distribution
         self.dist = dist
+        # Particle list
         self.particles = particles
         self.weights = weights
+        # Shapes
         self.s_shape = sample_shape
         self.b_shape = batch_shape
         self.e_shape = event_shape
@@ -159,45 +151,67 @@ class Message:
         if self.weights is not None:
             assert self.s_shape == self.weights.shape
 
+    def clone(self):
+        """
+            Return a cloned message from self. Guarantees that every tensor that constitutes self is cloned
+        """
+        msg_type = self.type
+        dist = None
+        particles = None
+        weights = None
+
+        if self.dist is not None:
+            params = Dist2Params.convert(self.dist)
+            params = params.clone()
+            dist = Params2Dist.convert(params, type(self.dist))
+        if self.particles is not None:
+            particles = self.particles.clone()
+        if self.weights is not None:
+            weights = self.weights.clone()
+
+        new_msg = Message(msg_type, self.s_shape, self.b_shape, self.e_shape, dist, particles, weights)
+        return new_msg
+
 
 # TODO general-inf: LinkData
 class LinkData:
     """
-        Identify the *data* of a ***directed*** link between a factor node and a variable node. Stores intermediate
-            messages in the **message memory**.
+        Identify the data of a directed link between a factor node and a variable node. Stores intermediate
+            messages in the message memory
         Note that links are directional, and two of such links should be specified with opposite directions to
             represent a bidirectional link between a factor node and a variable node, typically in the case of condacts.
-        During construction of the graph, its instance will be passed to `NetworkX` methods as the edge data to
+        During construction of the graph, its instance will be passed to NetworkX methods as the edge data to
             instantiate an edge.
     """
-
-    def __init__(self, vn, fn, var_list, to_fn, **kwargs):
+    def __init__(self, vn, fn, var_list, to_fn, epsilon, **kwargs):
         """
         :param vn:      name of the variable node that this link is incident to
         :param var_list:    list of variables of the adjacent variable node
         :param to_fn:   True/False indicating whether this link is pointing toward a factor node
         """
-        # Link message memory
+        assert isinstance(vn, VariableNode)
+        assert isinstance(fn, FactorNode)
+        assert isinstance(var_list, Iterable) and all(isinstance(var, Variable) for var in var_list)
+        assert isinstance(to_fn, bool)
+        assert isinstance(epsilon, float)
+
+        # Message memory, of type Message
         self.memory = None
-        # Whether this message is a new one just sent by adjacent node and haven't been read by the other node
+        # Whether this message is new, and haven't been read by recipient node. Of type bool
         self.new = False
-
-        # Following fields should correspond to the ones in the incident variable node
-        self.vn = vn
-        self.fn = fn
-        self.var_list = var_list
-
-        # Whether this link is pointing toward a factor node
+        # Incident nodes and their variable list
+        self.vn = vn                # Of type VariableNode
+        self.fn = fn                # Of type FactorNode
+        self.var_list = var_list    # Iterable of Variable
+        # Link direction. Whether pointing toward a factor node. Of type bool
         self.to_fn = to_fn
-
-        # Register additional attributes, if there are any. For example, a flag indicating whether this linkdata
-        #   contains negated message in an action conditional pattern.
+        # Threshold of KL-divergence metric to measure whether a candidate message is different from the existing one
+        self._epsilon = epsilon
+        # Reserved field for additional attributes. Arbitrary type
         self.attri = kwargs
-
-        # Record the dimensions of link message. Use to check potential dimension mismatch
-        self._dims = [var.size for var in self.var_list]
-
-        # Pretty log for GUI display
+        # Dimension of the link message. Determined from var_list. Of type torch.Size
+        self._dims = torch.Size([var.size for var in self.var_list])
+        # Pretty log
         self._pretty_log = {}
 
     def __str__(self):
@@ -209,35 +223,44 @@ class LinkData:
         else:
             return fn_name + " --> " + vn_name
 
-    def set(self, new, epsilon):
+    def set(self, new_msg, check_diff=True, clone=False):
         """
-            Set the link message memory to the new message arriving at this link. Implement the optimization so that
-                memory content is not changed if new message differs from existing content by less than epsilon
-            Default to check the absolute maximum difference between the old and new messages
-        :param new:         new arriving message
-        :param epsilon:     epsilon criterion
+            Set the link message memory to the new message arriving at this link.
+            If check_diff is True, then will check if the new message is different from the existing one before
+                replacing the existing with the new one.
+              If the two message are of different types, then they will be deemed as different. Otherwise, KL-divergence
+                will be used as the metric and its value to be compared with pre-set epsilon value. For Particles
+                messages, the particles will be assumed to be identical, and the weights will be compared by treating
+                them as probs to a Categorical distribution, from which the KL-divergence value is extracted.
+              The following KL formula is used:
+                    KL(old || new) = E[ log(old(x) / new(x) ],   x ~ old(x)
+            If clone is True, then will store a cloned new_msg
         """
-        # Check message type and size. In message is constant (int or float), CHANGE TO TENSOR
-        assert isinstance(new, (int, float, torch.Tensor))
-        if isinstance(new, (int, float)):
-            new = new * torch.ones(self._dims)
+        assert isinstance(new_msg, Message)
+
+        # If check_diff is False or no existing message or message types are different, will replace existing message
+        if self.memory is None or check_diff is False or new_msg.type != self.memory.type:
+            self.memory = new_msg.clone() if clone is True else new_msg
+            self.new = True
+            return
+
+        # Otherwise, check difference by KL-divergence
+        if new_msg.type in [MessageType.Tabular, MessageType.Distribution]:
+            # If message type is Tabular or Distribution, compute KL directly
+            p = self.memory.dist
+            q = new_msg.dist
         else:
-            size = list(new.shape)
-            if size != self._dims:
-                raise ValueError(
-                    "The new message's dimension '{}' does not match the link memory's preset dimension '{}'. "
-                    "Target variable node: '{}', link direction: toward factor node = '{}'"
-                        .format(size, self._dims, str(self.vn), self.to_fn))
+            # Otherwise, message type is Particles, so will temporarily instantiate Categorical distributions
+            p_logits = self.memory.weights
+            q_logits = new_msg.weights
+            p = Categorical(logits=p_logits)
+            q = Categorical(logits=q_logits)
+        val = kl(p, q)
 
-        # Check epsilon condition: maximal absolute difference < epsilon
-        #   TODO: allow other types of epsilon condition
-        if self.memory is not None:
-            diff = torch.max(torch.abs(self.memory - new))
-            if diff < epsilon:
-                return
-
-        self.memory = new
-        self.new = True
+        # Compare KL value with pre-set epsilon
+        if val > self._epsilon:
+            self.memory = new_msg.clone() if clone is True else new_msg
+            self.new = True
 
     def read(self):
         """
--- pysigma/utils.py
+++ pysigma/utils.py
@@ -21,14 +21,57 @@ def intern_name(name, struc_type):
         return "COND_[" + name.upper() + "]"
 
 
+# TODO general-inf: Utility methods for instantiate a distribution from the given list of tensors. The given list of
+#   tensors is treated as the parameters necessary to define that distribution. The semantics of the individual tensors
+#   in the list depends on the distribution.
+class Params2Dist:
+    # Lookup table for automatic conversion
+    type2method = \
+        {}
+
+    @classmethod
+    def convert(cls, params, dist_type):
+        """
+            Automatic conversion. Provide the params and the distribution class, return a distribution instance.
+        """
+        assert isinstance(params, list) and all(isinstance(param, torch.Tensor) for param in params)
+        assert issubclass(dist_type, torch.distributions.Distribution)
+        if dist_type not in cls.type2method.keys():
+            raise NotImplementedError("Conversion from parameters to distributions for distribution class {} has not"
+                                      "been implemented".format(dist_type))
+
+        return cls.type2method[dist_type](params)
+
+
+# TODO general-inf: Inversed version of the above class
+class Dist2Params:
+    # Lookup table for automatic conversion
+    type2method = \
+        {}
+
+    @classmethod
+    def convert(cls, dist):
+        """
+            Automatic conversion. Provide the distribution instance, return its params as list of tensors.
+        """
+        assert isinstance(dist, torch.distributions.Distribution)
+        dist_type = type(dist)
+        if dist_type not in cls.type2method.keys():
+            raise NotImplementedError("Conversion from distributions to parameters for distribution class {} has not"
+                                      "been implemented".format(dist_type))
+
+        return cls.type2method[dist_type](dist)
+
+
 # TODO general-inf: Utility methods for extracting natural parameters from an exponential torch.distributions, or
 #  instantiate an exponential torch.distribution from given natural parameters
 #  Note that, a new distribution will be instantiated with parameter tensors cloned from the provided ones. This is to
 #  protect the distribution instance from changes in the provided tensor. However .clone() retains the cloned tensor in
 #  the computation graph, so that gradients can still flow to the provided tensor.
-class Params2Dist:
+class Natural2Exp:
     pass
 
 
-class Dist2Params:
+class Exp2Natural:
     pass
+
