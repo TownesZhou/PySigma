9e633644d379b73af4e2033a80934f79e20676ca
Jincheng Zhou <jinchenz@usc.edu>
2020-11-05 14:37:07 -0800

    Testing for utils::compatible_shape()

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -67,3 +67,43 @@ def test_extern_name_correct():
     name, struc_type = "COND_[Test_name]", "conditional"
     expected_name = "Test_name"
     assert extern_name(name, struc_type) == expected_name
+
+
+def test_compatible_shape_invalid_arguments():
+    shape1 = None
+    shape2 = None
+    with pytest.raises(AssertionError):
+        compatible_shape(shape1, shape2)
+
+    shape1 = (None, None, None, None)
+    shape2 = (None, None, None, None)
+    with pytest.raises(AssertionError):
+        compatible_shape(shape1, shape2)
+
+    shape1 = (Size([1]), Size([2]), Size([3]),)
+    shape2 = (Size([1]), Size([2]), Size([3]), Size([4]))
+    with pytest.raises(AssertionError):
+        compatible_shape(shape1, shape2)
+
+
+def test_compatible_shape_compatible():
+    shape1 = (Size([1]), Size([2]), Size([3]), Size([4]))
+    shape2 = (Size([1]), Size([2]), Size([3]), Size([4]))
+    assert compatible_shape(shape1, shape2)
+
+    shape1 = (Size([1, 2]), Size([2, 3]), Size([3, 4]), Size([4, 5]))
+    shape2 = (Size([1, 2]), Size([2, 3]), Size([3, 4]), Size([4, 5]))
+    assert compatible_shape(shape1, shape2)
+
+    shape1 = (Size([]), Size([2]), Size([3]), Size([4]))
+    shape2 = (Size([1, 2]), Size([2]), Size([3]), Size([4]))
+    assert compatible_shape(shape1, shape2)
+
+def test_compatible_shape_incompatible():
+    shape1 = (Size([1]), Size([2]), Size([3]), Size([4]))
+    shape2 = (Size([1]), Size([2]), Size([3]), Size([5]))
+    assert not compatible_shape(shape1, shape2)
+
+    shape1 = (Size([1]), Size([2]), Size([3]), Size([4, 5]))
+    shape2 = (Size([1]), Size([2]), Size([3]), Size([5, 4]))
+    assert not compatible_shape(shape1, shape2)
