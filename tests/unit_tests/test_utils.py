444e657397b391f7abfd6353831d939008df6bc7
Jincheng Zhou <jinchenz@usc.edu>
2020-11-05 15:38:54 -0800

    Initial top-level mocking-based test suite for utils.DistributionServer.param2dist()

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -2,14 +2,17 @@
     Unit tests for pysigma.utils module
 """
 import pytest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, PropertyMock, patch
 import torch
 from torch import Size
 import torch.distributions as D
 
-from pysigma.utils import intern_name, extern_name, compatible_shape, DistributionServer, KnowledgeServer
+from pysigma.utils import intern_name, extern_name, compatible_shape
+from pysigma.utils import DistributionServer as DS
+from pysigma.utils import KnowledgeServer as KS
 
 
+# region
 def test_intern_name_invalid():
     name, struc_type = "Test", "something else"
     with pytest.raises(AssertionError):
@@ -55,6 +58,7 @@ def test_extern_name_invalid():
     with pytest.raises(AssertionError):
         extern_name(name, struc_type)
 
+
 def test_extern_name_correct():
     name, struc_type = "TYPE_[Test_name]", "type"
     expected_name = "Test_name"
@@ -99,6 +103,7 @@ def test_compatible_shape_compatible():
     shape2 = (Size([1, 2]), Size([2]), Size([3]), Size([4]))
     assert compatible_shape(shape1, shape2)
 
+
 def test_compatible_shape_incompatible():
     shape1 = (Size([1]), Size([2]), Size([3]), Size([4]))
     shape2 = (Size([1]), Size([2]), Size([3]), Size([5]))
@@ -107,3 +112,65 @@ def test_compatible_shape_incompatible():
     shape1 = (Size([1]), Size([2]), Size([3]), Size([4, 5]))
     shape2 = (Size([1]), Size([2]), Size([3]), Size([5, 4]))
     assert not compatible_shape(shape1, shape2)
+
+# endregion
+
+
+class TestDistributionServer:
+
+    # region
+    # Test top-level methods using mocks
+    def test_param2dist_dist_class_not_found(self):
+        # Test that the specified dist class cannot be found in the class's registry
+        dist_class = D.Distribution
+        param = torch.randn([3, 5])
+
+        # Patch the class attribute to an empty dictionary
+        with patch("pysigma.utils.DistributionServer.dict_param2dist", new_callable=PropertyMock) as d:
+            d.return_value = {}
+            with pytest.raises(NotImplementedError):
+                DS.param2dist(dist_class, param)
+
+    def test_param2dist_dist_wrong_shape(self):
+        # Test that the returned dist instance has a different shape than expected
+        dist_class = D.Distribution
+        random_param = torch.randn([3, 5])
+        b_shape, e_shape = Size([3]), Size([5])
+        dist = MagicMock(spec_set=D.Distribution)       # Mock a dist instance
+        # dist.batch_shape = PropertyMock(return_value=Size([1]))
+        # dist.event_shape = PropertyMock(return_value=Size([5]))
+        dist.batch_shape = Size([3, 5])
+        dist.event_shape = Size([])
+
+        with patch("pysigma.utils.DistributionServer.dict_param2dist", new_callable=PropertyMock) as d:
+            # Mock the sub-level class method that are returned by looking up the class registry
+            mock_callable = MagicMock(side_effect=lambda param, dist_info : dist)
+            # Set content of mocked class registry dictionary
+            d.return_value = {dist_class: mock_callable}
+
+            with pytest.raises(ValueError):
+                DS.param2dist(dist_class, random_param, b_shape=b_shape, e_shape=e_shape)
+
+    def test_param2dist_correct(self):
+        # Test correct return value
+        dist_class = D.Distribution
+        random_param = torch.randn([3, 5])
+        b_shape, e_shape = Size([3]), Size([5])
+        dist = MagicMock(spec_set=D.Distribution)  # Mock a dist instance
+        # dist.batch_shape = PropertyMock(return_value=Size([1]))
+        # dist.event_shape = PropertyMock(return_value=Size([5]))
+        dist.batch_shape = Size([3])
+        dist.event_shape = Size([5])
+
+        with patch("pysigma.utils.DistributionServer.dict_param2dist", new_callable=PropertyMock) as d:
+            # Mock the sub-level class method that are returned by looking up the class registry
+            mock_callable = MagicMock(side_effect=lambda param, dist_info: dist)
+            # Set content of mocked class registry dictionary
+            d.return_value = {dist_class: mock_callable}
+
+            assert DS.param2dist(dist_class, random_param, b_shape=b_shape, e_shape=e_shape) is dist
+
+    def test_dist2param(self):
+        pass
+
+    # endregion
