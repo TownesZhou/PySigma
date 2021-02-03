327a00a1134718aa4870efbe844bb9f7461187cb
TownesZhou <jinchenz@usc.edu>
2021-02-02 22:40:29 -0800

    Import optimization for test_AlphaFactorNode.py

--- tests/unit_tests/graphical/test_AlphaFactorNode.py
+++ tests/unit_tests/graphical/test_AlphaFactorNode.py
@@ -3,19 +3,10 @@
 """
 import pytest
 from unittest.mock import MagicMock
-import torch
-import torch.distributions as D
-import torch.distributions.constraints as C
-from torch import Size
 
-from pysigma.defs import Message, MessageType, Variable, VariableMetatype
-from pysigma.graphical.basic_nodes import LinkData, VariableNode, FactorNode, NodeConfigurationError
+from pysigma.defs import Message, Variable
+from pysigma.graphical.basic_nodes import LinkData, VariableNode, NodeConfigurationError
 from pysigma.graphical.alpha_beta_nodes import AlphaFactorNode
-from pysigma.utils import KnowledgeServer as KS
-from pysigma.utils import DistributionServer as DS
-from ...utils import random_message
-
-from ...utils import generate_positive_definite
 
 
 # Concrete subclass for testing
