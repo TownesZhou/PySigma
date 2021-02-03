40c2c906f2fa8166a3d66c1888a9c9eb4dfd24c2
TownesZhou <jinchenz@usc.edu>
2021-02-03 10:26:16 -0800

    Use conftest.py to configure global session-scoped pytest fixture for setting pytorch random seed.

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+"""
+    Global pytest configurations. Contains session-scoped fixtures for pretest setup.
+"""
+import pytest
+import torch
+
+
+# Declare autouse=True to execute at the beginning of tests and need not be referenced in test methods
+@pytest.fixture(scope="session", autouse=True)
+def set_torch_random_seed():
+    # Set pytorch random seed
+    torch.manual_seed(5)
+
